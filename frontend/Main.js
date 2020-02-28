b45ce9a482a683931ebed0169237b766daa6b52c
irfanf66 <m.irfan.fakhri66@gmail.com>
2020-02-28 14:31:42 +0700

    add multiplayer gameplay

--- frontend/Main.js
+++ frontend/Main.js
@@ -1,12 +1,16 @@
 class Main {
     constructor() {
-        this.circleR = 180;
+        this.circleR = 300;
         this.circleX = 0;
         this.circleY = 0;
         this.users = {};
         this.total_user = 0;
 
         this.ball = {};
+
+        this.player = {};
+
+        this.is_get_hit = false;
     }
 
     init(users, total_user, ball) {
@@ -20,6 +24,53 @@ class Main {
     clearRect() {
         ctx.clearRect(0, 0, canvas.offsetWidth, canvas.offsetHeight);
     }
+
+    render() {
+        this.clearRect();
+        this.drawPlatform();
+
+        let i = 0;
+
+        let users = this.users;
+
+        for (let k in users) {
+            let user = users[k];
+            if (user.hp > 0) {
+                let res = this.drawByAngle(360 * i / this.total_user, user.id);
+                user.x = res.x;
+                user.y = res.y;
+            }
+            i++;
+        }
+
+        this.drawBall();
+
+        this.doHit();
+
+        let player = this.users[this.player.id];
+
+        if (player.hp > 0) {
+            let rectW = 120 / 2;
+            let rectH = 80 / 2;
+
+            if (this.checkBallCollision({
+                x1: player.x - rectW,
+                y1: player.y - rectH,
+                x2: player.x + rectW,
+                y2: player.y + rectH,
+            })) {
+                collidedWithBall();
+            }
+            this.checkHp();
+        }
+    }
+
+    checkHp() {
+        if (this.users[this.player.id].hp === 0) {
+            alert('game-over')
+        }
+    }
+
 
     drawPlatform() {
         let circleR = this.circleR;
@@ -36,18 +87,36 @@ class Main {
         ctx.closePath();
     }
 
-    drawByAngle(angle) {
+    drawByAngle(angle, id) {
         let rad = angle * Math.PI / 180;
 
         let x = (this.circleR / 2) * 2 * Math.cos(rad) + this.circleX;
         let y = (this.circleR / 2) * 2 * Math.sin(rad) + this.circleY;
+        let rectW = 120;
+        let rectH = 80;
 
-        let rectR = 100;
+        // ctx.beginPath();
+        // ctx.rect(x, y, rectR, rectR);
+        // ctx.fillStyle = "#f00";
+        // ctx.fill();
+        // ctx.closePath();
+
         ctx.save();
         ctx.beginPath();
-        // ctx.rotate(rad);
-        ctx.rect(x - rectR / 2, y - rectR / 2, rectR, rectR);
-        ctx.fillStyle = "#000";
+        ctx.translate(x, y);
+        ctx.rotate(rad);
+        ctx.rect(-200 / 2, -200 / 2, 200, 200);
+        ctx.fillStyle = this.hitting && id === this.player.id ? "#0f0" : "#f00";
+        ctx.fill();
+        ctx.closePath();
+        ctx.restore();
+
+        ctx.save();
+        ctx.beginPath();
+        ctx.translate(x, y);
+        ctx.rotate(rad);
+        ctx.rect(-rectW / 2, -rectH / 2, rectW, rectH);
+        ctx.fillStyle = id === this.player.id ? "#00f" : "#000";
         ctx.fill();
         ctx.closePath();
         ctx.restore();
@@ -58,40 +127,78 @@ class Main {
         }
     }
 
-    render() {
-        this.clearRect();
-        this.drawPlatform();
-
-        let i = 0;
-
-        let users = this.users;
-
-        for (let k in users) {
-            let user = users[k];
-            let res = this.drawByAngle(360 * i / this.total_user);
-            user.x = res.x;
-            user.y = res.y;
-            i++;
-        }
-
-        this.drawBall();
-    }
-
-
     drawBall() {
         let rad = this.ball.angle * Math.PI / 180;
 
         let circleRBall = this.circleR + 50;
 
-        let ball_x = (circleRBall / 2) * 2 * Math.cos(rad) + this.circleX;
-        let ball_y = (circleRBall / 2) * 2 * Math.sin(rad) + this.circleY;
+        this.ball.x = (circleRBall / 2) * 2 * Math.cos(rad) + this.circleX;
+        this.ball.y = (circleRBall / 2) * 2 * Math.sin(rad) + this.circleY;
 
-        let r = 20;
+        this.ball.r = 15;
 
         ctx.beginPath();
-        ctx.arc(ball_x, ball_y, r, 0, Math.PI * 180);
+        ctx.arc(this.ball.x, this.ball.y, this.ball.r, 0, Math.PI * 180);
         ctx.fillStyle = "#F07575";
         ctx.fill();
         ctx.closePath();
+    }
+
+    hit() {
+        if (!this.hitting) {
+            this.hitting = true;
+
+            setTimeout(() => {
+                this.hitting = false;
+            }, 500);
+        }
+    }
+
+    doHit() {
+        let player = this.users[this.player.id];
+
+        let player_radius = 80;
+
+        if (this.hitting && !this.ball_got_hit) {
+            if (this.checkBallCollision({
+                x1: player.x - player_radius,
+                y1: player.y - player_radius,
+                x2: player.x + player_radius,
+                y2: player.y + player_radius,
+            })) {
+                this.ball_got_hit = true;
+                console.log(12391239123);
+                playerHit();
+            }
+        }
+
+        if (this.ball_got_hit) {
+            if (!this.checkBallCollision({
+                x1: player.x - player_radius,
+                y1: player.y - player_radius,
+                x2: player.x + player_radius,
+                y2: player.y + player_radius,
+            })) {
+                setTimeout(() => {
+                    this.ball_got_hit = false;
+                }, 200);
+            }
+        }
+    }
+
+    checkBallCollision(player) {
+        let ball = this.ball;
+
+        if (
+            ball.x + ball.r > player.x1 && ball.y + ball.r > player.y1 &&
+            ball.x < player.x2 && ball.y < player.y2
+        ) {
+            if (this.is_get_hit) return false;
+            this.is_get_hit = true;
+            return true;
+        }
+
+        this.is_get_hit = false;
+        return false;
     }
 }
--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
-// app.get('/', function (req, res) {
-//     res.sendFile(__dirname + '/frontend/index.html');
-// });
+app.use(express.static(__dirname + '/frontend'));
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/frontend/index.html');
+});
 
 let data = {};
 let total_users = 0;
@@ -13,8 +16,6 @@ io.on('connection', function (socket) {
 
     socket.on('join', function (msg) {
         socket.room_id = msg.room;
-
-        let rooms = Object.keys(socket.rooms);
 
         socket.join(msg.room);
 
@@ -29,26 +30,48 @@ io.on('connection', function (socket) {
                     to_right: true,
                 },
                 interval: null,
+                ready: false,
             };
 
         data[msg.room].users[this.id] = {
+            id: this.id,
             name: msg.name,
+            hp: 5,
         };
         data[msg.room].total_user++;
 
-        // io.to(msg.room).emit('connected', {
-        //     status: 'success',
-        //     data: data[msg.room],
-        // });
+        socket.emit('connected', {
+            status: 'success',
+            data: data[socket.room_id],
+            player: data[socket.room_id].users[this.id]
+        });
+    });
 
-        if (!data[msg.room].interval) {
-            data[msg.room].interval = setInterval(() => {
+    socket.on('ready', function () {
+        data[socket.room_id].users[this.id].ready = true;
+
+        let users = data[socket.room_id].users;
+
+        let total_ready = 0;
+
+        for (let k in users) {
+            let user = users[k];
+            total_ready += user.ready ? 1 : 0;
+        }
+
+        if (total_ready === data[socket.room_id].total_user && !data[socket.room_id].interval) {
+            data[socket.room_id].ready = true;
+            data[socket.room_id].interval = setInterval(() => {
                 let d = data[socket.room_id];
 
                 if (d) {
-                    d.ball.angle += d.ball.to_right ? 3 : -3;
+                    d.ball.angle += d.ball.to_right ? 2 : -2;
 
-                    io.to(socket.room_id).emit('connected', {
+                    io.to(socket.room_id).emit('start-game', {});
+
+                    console.log(this.id)
+
+                    io.to(socket.room_id).emit('render', {
                         status: 'success',
                         data: data[socket.room_id],
                     });
@@ -57,23 +80,36 @@ io.on('connection', function (socket) {
         }
     });
 
+    socket.on('hit', function (msg) {
+        data[socket.room_id].ball.to_right = data[socket.room_id].ball.to_right !== true;
+    });
+
+    socket.on('got-hit', function (msg) {
+        data[socket.room_id].users[this.id].hp--;
+    });
+
     socket.on('disconnecting', function () {
         let rooms = Object.keys(socket.rooms);
 
         if (data[socket.room_id]) {
-            delete data[socket.room_id].users[this.id];
+            if (!data[socket.room_id].ready) delete data[socket.room_id].users[this.id];
             data[socket.room_id].total_user--;
             if (data[socket.room_id].total_user == 0) {
                 clearInterval(data[socket.room_id].interval);
                 data[socket.room_id].interval = null;
                 delete data[socket.room_id];
+                return 0;
             }
+
+            // if (data[socket.room_id] && data[socket.room_id].users[this.id]) {
+            // io.to(socket.room_id).emit('render', {
+            //     status: 'success',
+            //     data: data[socket.room_id],
+            //     player: data[socket.room_id].users[this.id],
+            // });
+            // }
         }
 
-        io.to(socket.room_id).emit('connected', {
-            status: 'success',
-            data: data[socket.room_id],
-        });
 
         console.log('disconnected');
     });
